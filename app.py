10d8d1df12162c5e316927ecdf401a3c392c1704
ola-german-elevation <ola@elevationacademy.co>
2019-11-16 22:15:58 +0200

    bla3

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://heroku_4ctp328z:DiscWorld12@ds041228.mlab.com:41228/heroku_4ctp328z"
 mongo = PyMongo(app)
-mongo = mongo['heroku_4ctp328z']
-
+db = mongo['heroku_4ctp328z']
+my_rasps = db['rasps']
 
 @app.route('/')
 def hello():
@@ -19,7 +19,7 @@ def hello():
 
 @app.route('/index')
 def index():
-    raspberries = mongo['rasps'].find()
+    raspberries = my_rasps.find()
     return render_template('index.html', title='Home', raspberries=raspberries)
 
 
@@ -28,12 +28,12 @@ def saveip():
     name = request.args.get('name')
     ip = request.args.get('ip')
     mydict = {'name': name, 'ip': ip}
-    mongo['rasps'].insert_one(mydict)
+    my_rasps.insert_one(mydict)
     return "ok"
 
 @app.route('/clearlist')
 def clear_all_rasp():
-    mongo['rasps'].delete_many({})
+    my_rasps.delete_many({})
     return "The list is cleared"
 
 if __name__ == '__main__':
