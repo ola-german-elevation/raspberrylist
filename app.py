75364cd33bff7aab854d92bb6dce9c78cd95c74a
ola-german-elevation <ola@elevationacademy.co>
2019-11-16 20:00:55 +0200

    Done stage 2 with DB +req

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-# ips = {'raspberry1501':'10.0.0.7', 'raspberry1502':'10.0.0.3' }   # 'name':'ip'
-ips = {}
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def hello():
     return "Raspberry pi List of working ips"
 
+
 @app.route('/index')
 def index():
-    raspberries = [{'name':name, 'ip': ips[name]} for name in ips ]
-
+    raspberries = mongo.db['rasps'].find()
     return render_template('index.html', title='Home', raspberries=raspberries)
 
 
@@ -22,12 +23,14 @@ def index():
 def saveip():
     name = request.args.get('name')
     ip = request.args.get('ip')
-    try:
-        ips[name] = ip
-    except Exception as e:
-        print(e)
-
+    mydict = {'name': name, 'ip': ip}
+    mongo.db['rasps'].insert_one(mydict)
     return "ok"
+
+@app.route('/clearlist')
+def clear_all_rasp():
+    mongo.db['rasps'].delete_many({})
+    return "The list is cleared"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
